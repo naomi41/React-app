5e6602aa77cb3a7b15aa5fc0c5335d2d86b53112
naomi41 <naomirozenberg3@gmail.com>
2021-07-02 12:23:35 +0300

    working with axios (Prototype V)

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,12 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState } from 'react';
+import axios from "axios";
 
 export default function App() {
+    const [city, setCity] = useState(null);
+  const [ul, setUl] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = `a8f5a22819d25df63838b32e0cf4b2f4`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showTemperature);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  function showTemperature(response) {
+    console.log(response);
+    setUl(
+      <ul>
+        <li>Temperature: {Math.round(response.data.main.temp)}°C</li>
+        <li>Description: {response.data.weather[0].description} </li>
+        <li>Humidity: {Math.round(response.data.main.humidity)} %</li>
+        <li>Wind: {Math.round(response.data.wind.speed)}km/h</li>
+        <li>
+          <img
+            src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+          ></img>
+        </li>
+      </ul>
+    );
+  }
   return (
-    <div className="App">
-      <h1>Welcome</h1>
+    <div className="Form">
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Enter a city" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
+      <div>{ul}</div>
     </div>
   );
 }
+
+
+
 
 
