f2f1c57d43bdf89d30948683d41103171a0db5b5
naomi41 <naomirozenberg3@gmail.com>
2021-07-04 09:52:33 +0300

    cars Deleted

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Form from './Form';
-import Card from './Card';
+
 function Index() {
   return (
     <div className="Index">
    <Form />
-   <Card/>
     </div>
   );
 }
